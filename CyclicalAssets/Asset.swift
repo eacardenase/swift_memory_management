e38f9b0dc2f24b5e8db374b7574a7add297fc133
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-07 18:42:12 -0500

    feat: add asset reference type

---
+++ CyclicalAssets/Asset.swift
@@ -0,0 +1,33 @@
+//
+//  Asset.swift
+//  CyclicalAssets
+//
+//  Created by Edwin Cardenas on 5/7/25.
+//
+
+import Foundation
+
+class Asset {
+    let name: String
+    let value: Double
+    var container: Vault?
+    
+    init(name: String, value: Double) {
+        self.name = name
+        self.value = value
+    }
+    
+    deinit {
+        print("\(self) is being deallocated")
+    }
+}
+
+extension Asset: CustomStringConvertible {
+    var description: String {
+        if let container = container {
+            return "Asset(\(name), worth \(value), in \(container))"
+        } else {
+            return "Asset(\(name), worth \(value), not stored anywhere)"
+        }
+    }
+}
--- CyclicalAssets/main.swift
+++ CyclicalAssets/main.swift
@@ -11,6 +11,12 @@ class Simulation {
     func run() {
         let vault13 = Vault(number: 13)
         print("Created \(vault13)")
+        
+        let coin = Asset(name: "Rare Coin", value: 1_000.0)
+        let gem = Asset(name: "Big Diamond", value: 5_000.0)
+        let poem = Asset(name: "Magnum Opus", value: 0.0)
+        
+        print("Created some assets: \([coin, gem, poem])")
     }
 }
 
