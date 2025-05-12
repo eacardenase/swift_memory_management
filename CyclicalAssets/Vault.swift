710e337d0597644e3660d30c80d4d93ae23da4c9
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-12 18:43:51 -0500

    feat: use unowned reference to break retain cycle (use with caution)

--- CyclicalAssets/Vault.swift
+++ CyclicalAssets/Vault.swift
@@ -27,8 +27,8 @@ class Vault {
     func store(_ asset: Asset) {
         asset.container = self
         
-        asset.changeHandler = { [weak self] (change) in
-            print("An asset has changed value by \(change). New total value: \(String(describing: self?.totalValue))")
+        asset.changeHandler = { [unowned self] (change) in
+            print("An asset has changed value by \(change). New total value: \(totalValue)")
         }
         
         assets.append(asset)
